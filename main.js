9beed695845947ce97bf67efa94eb07d90c7fa17
Gian Vieceli <gianvieceli@gmail.com>
2024-12-24 15:31:45 -0300

    proj 7 calculadora celcius pra farhenheith

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-let firstName = prompt('digite seu primeiro nome')
-let greeting = "Hello "
+// let firstName = prompt('digite seu primeiro nome')
+// let greeting = "Hello "
 // let lastName = prompt('digite seu sobrenome')
 // let age = parseInt(prompt('digite sua idade'))
 
-let friends = ['ana', 'bruno', 'julia', 'bela']
+//ptoj 5
 
-console.log(friends[1])
-friends[1] = 'bruuuunin'//replace
-console.log(friends[4])
-friends[4] = 'junin'//add item
-
+// let firstName = prompt('digite seu primeiro nome')
+// let greeting = "Hello "
 // console.log(`idade ${age}`)
-document.getElementById('greeting').innerHTML = greeting + firstName
+// document.getElementById('greeting').innerHTML = greeting + firstName
+
+
+//proj 6 array
+
+// let friends = ['ana', 'bruno', 'julia', 'bela']
+
+// console.log(friends[1])
+// friends[1] = 'bruuuunin'//replace
+// console.log(friends[4])
+// friends[4] = 'junin'//add item
+
+
+//proj 7 calculadora C° --> F°
+
+
+let celcius = 0
+let fahrenheith = 0
+
+celcius = parseFloat(prompt('temperatura em celcius'))
+fahrenheith = celcius * 1.8 + 32
+
+alert("a temperatura em farhenheith é de " + fahrenheith.toFixed(2))
