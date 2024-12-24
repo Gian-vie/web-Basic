375e82bab389fac7fd4269c32f1a2b870eb945dc
Gian Vieceli <gianvieceli@gmail.com>
2024-12-24 15:20:10 -0300

    proj 6 array

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ let greeting = "Hello "
 // let lastName = prompt('digite seu sobrenome')
 // let age = parseInt(prompt('digite sua idade'))
 
-// console.log(`idade ${age}`)
+let friends = ['ana', 'bruno', 'julia', 'bela']
 
+console.log(friends[1])
+friends[1] = 'bruuuunin'//replace
+console.log(friends[4])
+friends[4] = 'junin'//add item
+
+// console.log(`idade ${age}`)
 document.getElementById('greeting').innerHTML = greeting + firstName
