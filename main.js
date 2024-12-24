f541c62bfbd71f8ad4d98362d8bfcc7fbbcad402
Gian Vieceli <gianvieceli@gmail.com>
2024-12-24 14:57:27 -0300

    proj 5 (n foi feito commits anteriores)

---
+++ main.js
@@ -0,0 +1,8 @@
+let firstName = prompt('digite seu primeiro nome')
+let greeting = "Hello "
+// let lastName = prompt('digite seu sobrenome')
+// let age = parseInt(prompt('digite sua idade'))
+
+// console.log(`idade ${age}`)
+
+document.getElementById('greeting').innerHTML = greeting + firstName
